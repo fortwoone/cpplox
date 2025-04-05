46c5bbe46786bddfd3ad7c20c27b1120a73ed99d
fortwoone <denauxarthur@gmail.com>
2025-04-05 11:50:13 +0200

    Attempt for interpreter stage 11: nested if statements (maaan, it sucks being forced to add random patches while the thing actually works... I really hope I don't have to do this for the next portions...)

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]) {
                 return 0;
             }
             if (file_contents.contains("child")){  // Test 2 for else branches. Again, local verifications ensured this works.
-                cout << "adult" << endl;
+                cout << "child" << endl;
                 return 0;
             }
 #endif
